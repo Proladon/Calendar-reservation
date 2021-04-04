21ff59e666e8cd55e68752da2e0cf242141d8792
Proladon <proladon@gmail.com>
2021-04-04 19:57:14 +0800

    headbar layout

--- src/components/Headbar.vue
+++ src/components/Headbar.vue
@@ -1,16 +1,22 @@
 <template>
   <div class="headbar">
+        
     <div class="func-btn-container">
-      <p>預約行事曆</p>
+      <!-- <p>預約行事曆</p> -->
+      <input type="date" name="" id="">
+      <div class="cur-date">
+      </div>
+      <div class="view change-btn">日檢視 ▼</div>
+      <div class="user change-btn">江 ▼</div>
 
     </div>
     
     <div class="week-day-container">
       <div class="select-date-btn">💎</div>
       
-      <div class="day-btn" >
-        <div class="day-en-tile">M</div>
-        <div class="day-num">01</div>
+      <div class="day-btn" v-for="dTitle, index in dayEnTitle" :key="dTitle">
+        <div class="day-en-tile">{{dTitle}}</div>
+        <div class="day-num">0{{index + 1}}</div>
       </div>
 
     </div>
@@ -21,9 +27,12 @@
 <script>
 export default {
   name: 'Headbar',
-  props: {
-    msg: String
-  },
+  
+  data(){
+    return{
+      dayEnTitle: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
+    }
+  }
   
 }
 </script>
@@ -31,20 +40,32 @@ export default {
 <style scoped>
 .headbar{
   @apply flex flex-col justify-between;
-  @apply shadow-md h-1/6 p-4;
+  @apply shadow-md h-1/6 p-2;
 }
 
 .func-btn-container{
-  @apply flex ;
-  @apply bg-gray-300;
+  @apply flex justify-between;
 }
 
 .week-day-container{
   @apply flex justify-between;
-  @apply bg-gray-300;
 }
 
 .day-btn{
-  @apply flex flex-col text-gray-700;
+  @apply flex justify-center items-center;
+  @apply flex-col text-gray-700 text-sm;
+}
+
+.change-btn{
+  background-color: #DEDAF4;
+  @apply cursor-pointer rounded-full p-2 px-3 text-sm;
+}
+
+.cur-date{
+  flex: 2;
+}
+
+.view{
+  @apply mr-3;
 }
 </style>
