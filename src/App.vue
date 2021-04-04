6967cc0b4f5abc218798e859e97d09fec8d0253e
Proladon <proladon@gmail.com>
2021-04-04 19:27:12 +0800

    project init / tailwind

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <Headbar />
+  </div>
+</template>
+
+<script>
+import Headbar from './components/Headbar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Headbar
+  }
+}
+</script>
+
+<style>
+html, body, #app{
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ src/components/Headbar.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="headbar">
+    <div class="func-btn-container">
+      <p>預約行事曆</p>
+
+    </div>
+    
+    <div class="week-day-container">
+      <div class="select-date-btn">💎</div>
+      
+      <div class="day-btn" >
+        <div class="day-en-tile">M</div>
+        <div class="day-num">01</div>
+      </div>
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Headbar',
+  props: {
+    msg: String
+  },
+  
+}
+</script>
+
+<style scoped>
+.headbar{
+  @apply flex flex-col justify-between;
+  @apply shadow-md h-1/6 p-4;
+}
+
+.func-btn-container{
+  @apply flex ;
+  @apply bg-gray-300;
+}
+
+.week-day-container{
+  @apply flex justify-between;
+  @apply bg-gray-300;
+}
+
+.day-btn{
+  @apply flex flex-col text-gray-700;
+}
+</style>
